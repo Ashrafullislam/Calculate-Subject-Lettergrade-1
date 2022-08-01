16d624b52e419d024d0d5705900ac2cff060f3df
Md.Ashrafull Islam <ashrafull130@gmail.com>
2022-08-01 23:55:20 +0600

    Added the calculate of letter grade

---
+++ find-grade.js
@@ -0,0 +1,173 @@
+//   the programe make for find the letter grade of any subject:
+ var num1 = prompt("Enter your First subject mark  ");
+ var num2 = prompt("Enter your Second subject mark  ");
+ var num3 = prompt("Enter your  Third subject mark  ");
+ var num4 = prompt("Enter your Fourth subject mark  ");
+ var num5 = prompt("Enter your Fiveth subject mark  ");
+ var num6 = prompt("Enter your Sixth subject mark  ");
+// var num7 = prompt("Enter your Seven subject mark  ");
+// var num8 = prompt("Enter your Eighth subject mark  ");
+// var num9 = prompt("Enter your Nineth subject mark  ");
+// var num20 = prompt("Enter your Tenth subject mark  ");
+
+// its for first subject find the grade 
+
+   if(num1 >= 90 ){
+       document.write("Your first  marks: " + num1 + "and your grade is :" + "<b> A+</b> " )  
+   }
+   else if( num1 >= 80 ){
+       document.write("Your first marks: " + num1 + "and your grade is :" + "<b> A </b> " )  
+   }
+   else if( num1 >= 70 ){
+       document.write("Your first marks: " + num1 + "  " + "and your grade is :" + "<b> A - </b> " )  
+   }
+   else if( num1 >= 60 ){
+       document.write("Your first marks: " + num1 + "  " +  "and your grade is :" + "<b> B </b> " )  
+   }
+   else if( num1 >=50 ){
+       document.write("Your first marks: " + num1 + "  " +  "and your grade is :" + "<b> C </b> " )  
+   }
+   else if( num1 >= 34 ){
+       document.write("Your first marks: " + num1 + "  " +  "and your grade is :" + "<b> D </b> " )  
+   }
+   else if (num1 <= 33 ){
+       document.write("Your first grade is: "  + "<b>F</b>" );
+   }  
+
+   document.write("<br>");
+
+    //  its for second subject find the grade  
+   if(num2 >= 90 ){
+       document.write("Your second marks: " + num2 + "and your grade is :" + "<b> A+</b>" )  
+   }
+   else if( num2 >= 80 ){
+       document.write("Your second marks: " + num2 + "and your grade is :" + "<b> A </b> " )  
+   }
+   else if( num2 >= 70 ){
+       document.write("Your second marks: " + num2 + "  " + "and your grade is :" + "<b> A - </b> " )  
+   }
+   else if( num2 >= 60 ){
+       document.write("Your second marks: " + num2 + "  " +  "and your grade is :" + "<b> B </b> " )  
+   }
+   else if( num2 >=50 ){
+       document.write("Your second marks: " + num2 + "  " +  "and your grade is :" + "<b> C </b> " )  
+   }
+   else if( num2 >= 34 ){
+       document.write("Your second marks: " + num2 + "  " +  "and your grade is :" + "<b> D </b> " )  
+   }
+   else if (num2 <= 33 ){
+       document.write("Your second grade is: "  + "<b>F</b>" );
+   } 
+
+   document.write("<br>");
+
+    // its for subject three find the grade  
+   if(num3 >= 90 ){
+       document.write("Your third marks: " + num3 + "and your grade is :" + "<b> A+</b>" )  
+   }
+   else if( num3 >= 80 ){
+       document.write("Your third marks: " + num3 + "and your grade is :" + "<b> A </b> " )  
+   }
+   else if( num3 >= 70 ){
+       document.write("Your third marks: " + num3 + "  " + "and your grade is :" + "<b> A - </b> " )  
+   }
+   else if( num3 >= 60 ){
+       document.write("Your third marks: " + num3 + "  " +  "and your grade is :" + "<b> B </b> " )  
+   }
+   else if( num3 >=50 ){
+       document.write("Your third marks: " + num3 + "  " +  "and your grade is :" + "<b> C </b> " )  
+   }
+   else if( num3 >= 34 ){
+       document.write("Your third marks: " + num3 + "  " +  "and your grade is :" + "<b> D </b> " )  
+   }
+   else if (num3 <= 33 ){
+       document.write("Your third grade is: "  + "<b>F</b>" );
+   }
+    document.write("<br>");
+
+       
+    //  its for fourth subject find the grade   
+   if(num4 >= 90 ){
+       document.write("Your fourth marks: " + num4 + "and your grade is :" + "<b> A+</b>" )  
+   }
+   else if( num4 >= 80 ){
+       document.write("Your fourth  marks: " + num4 + "and your grade is :" + "<b> A </b> " )  
+   }
+   else if( num4 >= 70 ){
+       document.write("Your fourth marks: " + num4 + "  " + "and your grade is :" + "<b> A - </b> " )  
+   }
+   else if( num4 >= 60 ){
+       document.write("Your fourth marks: " + num4 + "  " +  "and your grade is :" + "<b> B </b> " )  
+   }
+   else if( num4 >=50 ){
+       document.write("Your fourth marks: " + num4 + "  " +  "and your grade is :" + "<b> C </b> " )  
+   }
+   else if( num4 >= 34 ){
+       document.write("Your fourth marks: " + num4 + "  " +  "and your grade is :" + "<b> D </b> " )  
+   }
+   else if (num4 <= 33 ){
+       document.write("Your fourth grade is: "  + "<b>F</b>" );
+   }
+   document.write("<br>");
+ 
+    //   its for find the fifth subject   
+   if(num5 >= 90 ){
+       document.write("Your fiveth marks: " + num5 + "and your grade is :" + "<b> A+</b>" )  
+   }
+   else if( num5 >= 80 ){
+       document.write("Your fiveth marks: " + num5 + "and your grade is :" + "<b> A </b> " )  
+   }
+   else if( num5 >= 70 ){
+       document.write("Your fiveth marks: " + num5 + "  " + "and your grade is :" + "<b> A - </b> " )  
+   }
+   else if( num5 >= 60 ){
+       document.write("Your fiveth marks: " + num5 + "  " +  "and your grade is :" + "<b> B </b> " )  
+   }
+   else if( num5 >=50 ){
+       document.write("Your fiveth marks: " + num5 + "  " +  "and your grade is :" + "<b> C </b> " )  
+   }
+   else if( num5 >= 34 ){
+       document.write("Your fiveth marks: " + num5 + "  " +  "and your grade is :" + "<b> D </b> " )  
+   }
+   else if (num5 <= 33 ){
+       document.write("Your fiveth grade is: "  + "<b>F</b>" );
+   }
+   document.write("<br>");
+
+
+
+// its for find the sixth subject grade 
+
+
+if(num6 >= 90 ){
+    document.write("Your Sixth marks: " + num6 + "and your grade is :" + "<b> A+</b>" );
+
+}
+else if( num6 >= 80 ){
+    document.write("Your Sixth  marks: " + num6 + "and your grade is :" + "<b> A </b> " );
+
+}
+
+else if( num6 >= 70 ){
+    document.write("Your Sixth marks: " + num6 + "  " + "and your grade is :" + "<b> A - </b> " );
+
+}
+else if( num6 >= 60 ){
+    document.write("Your Sixth marks: " + num6 + "  " +  "and your grade is :" + "<b> B </b> " );
+
+}
+
+else if( num6 >=50 ){
+    document.write("Your Sixth marks: " + num6 + "  " +  "and your grade is :" + "<b> C </b> " );
+
+}
+else if( num6 >= 34 ){
+    document.write("Your Sixth marks: " + num6 + "  " +  "and your grade is :" + "<b> D </b> " );
+
+}
+else if (num6 <= 33 ){
+    document.write("Your Sixth grade is: "  + "<b>F</b>" );
+}
+
+
+
